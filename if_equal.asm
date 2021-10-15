155b11f507793d17eb942fbfb105f7895026fe24
JingRyu <81526024+JingRyu@users.noreply.github.com>
2021-10-15 13:05:55 +0800

    Add files via upload

--- if_equal.asm
+++ if_equal.asm
@@ -4,40 +4,52 @@ INCLUDE Irvine32.inc
 
 ; (insert macro definitions here)
 
-RIGHT_NUMBER = 7
-
 .data
 
-greeting			BYTE	"Welcome here!", 13, 10, 0
-prompt_1			BYTE	"Pick between 1 and 10: ", 0
-userVal				DWORD	?		;User' Guess
-correct_string		BYTE	"Amazing!", 13, 10, "You won!", 0
-incorrect_string	BYTE	"Sorry,", 13, 10, "Better next time!", 0
+.data   ; Data Segment ALPHA
+x			SDWORD  ?
+y			SDWORD  ?
+z			SDWORD  ?
+prompt_k	byte	"k: ", 0
+prompt_n	byte	"n: ", 0
+prompt_x	byte	"x: ", 0
+prompt_y	byte	"y: ", 0
+prompt_z	byte	"z: ", 0
+
+yes			BYTE    "Yes",13,10,0
+no			BYTE    "No",13,10,0
+maybe		BYTE    "Maybe",13,10,0
 
 .code
 main PROC
-
-; Greet the user
-mov		edx, OFFSET greeting
-call	Writestring
-
 ; Get the user's guess
-mov		edx, OFFSET prompt_1
+mov		edx, OFFSET prompt_x
 call	Writestring
-call	ReadDec
-mov		userVal, eax
+call	ReadInt
+mov		x, eax
+mov		edx, OFFSET prompt_y
+call	Writestring
+call	ReadInt
+mov		y, eax
+mov		edx, OFFSET prompt_z
+call	Writestring
+call	ReadInt
+mov		z, eax
 
 ; Evaluate if correct and print appropriate reponse
-	cmp		eax, RIGHT_NUMBER
-	JE		_Correct
-	mov		edx, OFFSET incorrect_string
-	call	Writestring
+	mov		edx, x
+	CMP		edx, y
+	JL		_trueCase
+	cmp		edx, z
+	JG		_trueCase
+	mov		edx, OFFSET maybe
+	call	WriteSTring
 	JMP		_Finished
 
-
-_Correct:
-	mov		edx, OFFSET correct_string
+_trueCase:
+	mov		edx, OFFSET no
 	call	Writestring
+	JMP		_Finished
 
 _Finished:
 	Invoke ExitProcess,0	; exit to operating system
